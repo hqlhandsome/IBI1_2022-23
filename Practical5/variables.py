44e851ec48e92985641e0017e2b266815cbef4f3
hqlhandsome <127087270+hqlhandsome@users.noreply.github.com>
2023-03-14 16:49:59 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,1 +1,19 @@
-?
+Edinburgh is found at the longitude -3.19. Store this number in a variable called a.
+• Los Angeles is at -118.24. Store this number in a variable called b.
+Haining is found at the longitude 116.39. Store this number in a variable called c.
+• Calculate the longitude distance that Rob travelled to Los Angeles as the difference between a
+and b. Store this value in a variable called d.
+Calculate the longitude distance that Rob travelled to Haining as the difference between and a
+c. Store this value in a variable called e.
+a=-3.19
+b=-118.24
+c=116.39
+d=|a-b|
+e=|a-c|
+if d < e:
+  print ("Haining is further")
+elif e < d:
+  print ("Los Angeles is further")
+else:
+  print ("Same")
+ 

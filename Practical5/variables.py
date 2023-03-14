415540b72b54fe09c6e54b15faf2f8965ebba2da
hqlhandsome <127087270+hqlhandsome@users.noreply.github.com>
2023-03-14 21:43:44 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,25 +1,18 @@
-#a is the longitude of Edinburgh
-#b is the longitude of Los Angeles
-#c is the longitude of Haining
-#d is the longitude distance that Rob travelled to Los Angeles, as the difference between a and b
-#e is the longitude distance that Rob travelled to Haining, as the difference between a and c
-a=-3.19
-b=-118.24
-c=116.39
-d=abs(a-b)
-e=abs(a-c)
+a=-3.19 #a is the longitude of Edinburgh
+b=-118.24 #b is the longitude of Los Angeles
+c=116.39 #c is the longitude of Haining
+d=abs(a-b) #d is the longitude distance that Rob travelled to Los Angeles, as the difference between a and b
+e=abs(a-c) #e is the longitude distance that Rob travelled to Haining, as the difference between a and c
 if d < e:
   print ("Haining is further")
 elif e < d:
   print ("Los Angeles is further")
 else:
-  print ("Same")
-#Rob travel further to Haining than Los Angeles
+  print ("Same") #Rob travel further to Haining than Los Angeles
 
 X=True
 Y=False
 W=X and Y
 Z=X or Y
-print (W,Z)
-# W means false and Z means true
+print (W,Z) # W means false and Z means true
 

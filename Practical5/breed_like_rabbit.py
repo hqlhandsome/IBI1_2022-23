182e8c098c43a903ffd733b1c0573b30b65b1f31
hqlhandsome <127087270+hqlhandsome@users.noreply.github.com>
2023-03-15 15:35:05 +0800

    Update breed_like_rabbit.py

--- Practical5/breed_like_rabbit.py
+++ Practical5/breed_like_rabbit.py
@@ -1,6 +1,9 @@
-a=1 #a means the 
-m=2 #m means the
+#we can use the while-loop to calculate the rabbit number and add the limit to make sure the total number <=100
+#when the total number is over 100 the loop stops.
+a=1 #a means the generation of the rabbit
+m=2 #m means the total number of the rabbit in each generation
 while m<=100:
   a=a+1
-  m=2*m
-  print(a,m)
+  m=2*m #two rabbit will produce two new rabbits
+  print(a,m)#the number of the rabbit in each generation until we have at least 100 rabbits
+print(a,m)#the first number 7 is the generation, the second number 128 is the total number of the rabbit.

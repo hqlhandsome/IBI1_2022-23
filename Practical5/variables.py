5c592add95e332cd2c8af42b3e7c827f2cadf2fe
hqlhandsome <127087270+hqlhandsome@users.noreply.github.com>
2023-03-23 20:09:21 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,6 +3,7 @@ b=-118.24 #b is the longitude of Los Angeles
 c=116.39 #c is the longitude of Haining
 d=abs(a-b) #d is the longitude distance that Rob travelled to Los Angeles, as the difference between a and b
 e=abs(a-c) #e is the longitude distance that Rob travelled to Haining, as the difference between a and c
+# use if, elif, else to judge which distanse is greater
 if d < e:
   print ("Haining is further")
 elif e < d:

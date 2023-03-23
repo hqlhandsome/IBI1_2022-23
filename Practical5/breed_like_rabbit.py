644978e9aad0797f294146ba34dea2d37dcda467
hqlhandsome <127087270+hqlhandsome@users.noreply.github.com>
2023-03-23 19:43:52 +0800

    Update breed_like_rabbit.py

--- Practical5/breed_like_rabbit.py
+++ Practical5/breed_like_rabbit.py
@@ -6,4 +6,11 @@ while m<=100:
   a=a+1
   m=2*m #two rabbit will produce two new rabbits
   print(a,m)#the number of the rabbit in each generation until we have at least 100 rabbits
+  #1st generation has 2 rabbits
+  #2nd generation has 4 rabbits
+  #3rd generation has 8 rabbits
+  #4th generation has 16 rabbits
+  #5th generation has 32 rabbits
+  #6th generation has 64 rabbits
+  #7th generation has 128 rabbits
 print(a,m)#the first number 7 is the generation, the second number 128 is the total number of the rabbit.

c1558ff8bfd9a37a735e9b602595ce5b6a1e1151
hqlhandsome <127087270+hqlhandsome@users.noreply.github.com>
2023-03-23 20:02:55 +0800

    Update hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,6 +1,6 @@
 #We can use While-loop to calculate the the hexagon sequence and computes and displays the first five values of the hexagon sequence
 n=1 #n means the number of the hexagon
-while n<=5:
+while n<=5: #start the while loop and stop it when n>5
   h=2*n*(2*n-1)*0.5 # h means the number of the dots.
-  n=n+1
+  n=n+1 #calculate next h
   print(h) #h1=1.0 h2=6.0 h3=15.0 h4=28.0 h5=45.0
